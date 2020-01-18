4e1c13b267e6262c9fe929d309374c6d51a44b41
“Arpit <arpitgoyal437@gmail.com>
2020-01-18 12:19:24 +0530

    remove white space

--- RemoveWhiteSpace.java
+++ RemoveWhiteSpace.java
@@ -2,21 +2,25 @@ package com.company;
 
 import java.util.Scanner;
 
+import static com.company.Utils.println;
+import static com.company.Utils.print;
+
 public class RemoveWhiteSpace {
-    static void removeWhiteSpace(char[] inputStringArray) {
-        /*
+
+    /*
         removeWhiteSpace() method to remove the white space from an string
         Input: character array containing all the elements of an string
         Output: print the string without any whiteSpace
          */
+    static void removeWhiteSpace(char[] inputStringArray) {
         for (char stringChar : inputStringArray)
             if (stringChar != ' ')
-                System.out.print(stringChar);
+                print(stringChar);
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter any String which contain white space: ");
+        println("Enter any String which contain white space: ");
         String inputString = input.nextLine();
         // converting an string into character array
         char[] inputStringArray = inputString.toCharArray();
