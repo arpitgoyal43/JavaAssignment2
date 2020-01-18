6bbd15b33d1b0fbfa1ac1f1f94a8dd5d104fd9f1
“Arpit <arpitgoyal437@gmail.com>
2020-01-18 12:16:42 +0530

    second highest element

--- SecondHighest.java
+++ SecondHighest.java
@@ -2,13 +2,15 @@ package com.company;
 
 import java.util.Scanner;
 
+import static com.company.Utils.println;
+
 public class SecondHighest {
+    /*
+       secondHighest() method to get second highest element in an array
+       Input: An array set contain multiple elements
+       Output: Second Maximum value from the array set
+       */
     public void secondHighest(int[] arraySet, int arraySet_size) {
-        /*
-        secondHighest() method to get second highest element in an array
-        Input: An array set contain multiple elements
-        Output: Second Maximum value from the array set
-        */
         int firstMaximum = 0, secondMaximum = 0;
         for (int i = 0; i < arraySet_size; i++) {
             if (arraySet[i] > firstMaximum) {
@@ -17,20 +19,19 @@ public class SecondHighest {
             } else if (arraySet[i] > secondMaximum && arraySet[i] < firstMaximum) {
                 secondMaximum = arraySet[i];
             }
-
-            System.out.println("Second Highest number: " + secondMaximum);
+            println("Second Highest number: " + secondMaximum);
         }
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter number of elements you want to use: ");
+        println("Enter number of elements you want to use: ");
         int arraySet_size = input.nextInt();
         if (arraySet_size < 2)
-            System.out.println("Invalid size!");
+            println("Invalid size!");
         else {
             int[] arraySet = new int[arraySet_size];
-            System.out.println("Enter the elements in a sequence: ");
+            println("Enter the elements in a sequence: ");
             for (int i = 0; i < arraySet_size; i++) {
                 arraySet[i] = input.nextInt();
             }
