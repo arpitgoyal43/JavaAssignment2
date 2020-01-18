6e2818a4012d582b625fbb3b0e5526c9bf5e3228
“Arpit <arpitgoyal437@gmail.com>
2020-01-18 12:21:56 +0530

    duplicate elements

--- DuplicateElements.java
+++ DuplicateElements.java
@@ -1,30 +1,32 @@
 package com.company;
 
 import java.util.Scanner;
+import static com.company.Utils.println;
+import static com.company.Utils.print;
 
 public class DuplicateElements {
     public static void main(String[] args) {
         try {
             Scanner input = new Scanner(System.in);
-            System.out.println("Enter number of elements in array: ");
+            println("Enter number of elements in array: ");
             int arraySet_size = input.nextInt();
             //  Check for the size of array if less than 2, then there is no duplicate element
             if (arraySet_size < 2) {
-                System.out.println("No duplicate elements.");
+                println("No duplicate elements.");
             } else {
                 // Integer array declaration
                 int[] arraySet = new int[arraySet_size];
-                System.out.println("Enter the elements in a sequence: ");
+                println("Enter the elements in a sequence: ");
                 for (int i = 0; i < arraySet_size; i++) {
                     arraySet[i] = input.nextInt();
                 }
-                System.out.println("Duplicate elements: ");
+                println("Duplicate elements: ");
                 // calling a static method printDuplicate([arrayset])
                 DuplicateElements.printDuplicate(arraySet);
 
             }
         } catch (Exception e) {
-            System.out.println("Please enter a valid number!");
+            println("Please enter a valid number!");
         }
     }
 
@@ -37,6 +39,6 @@ public class DuplicateElements {
         for (int i = 0; i < arraySet.length; i++)
             for (int j = i + 1; j < arraySet.length; j++)
                 if (arraySet[i] == arraySet[j])
-                    System.out.print(arraySet[j] + ",");
+                    print(arraySet[j] + ",");
     }
 }
